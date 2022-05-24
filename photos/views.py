a9428902c6d5a16563dcd2af086713aa0bd38919
ludwig <murimiludwig@gmail.com>
2022-05-24 08:42:35 +0300

    creation of th staticfile folder

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,12 @@
+from unicodedata import category
 from django.shortcuts import render
-
+from .models import Category, Photo
 # Create your views here.
 
 
 def gallery(request):
+    categories = Category.objects.all()
+    context = {'categories': Category }
     return render(request, 'photos/gallery.html')
 
 def viewPhoto(request, pk):
--- photoshare/settings.py
+++ photoshare/settings.py
@@ -120,6 +120,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/'
+
+STATICFILES_DIR = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
