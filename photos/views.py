ab499c3702a6914bc96c282db880dae4385a06fc
ludwig <murimiludwig@gmail.com>
2022-05-29 21:39:10 +0300

    creation of the add photo session

--- photos/views.py
+++ photos/views.py
@@ -12,7 +12,13 @@ def gallery(request):
     return render(request, 'photos/gallery.html',context)
 
 def viewPhoto(request, pk):
-    return render(request, 'photos/photo.html', pk)
+    photo = Photo.objects.all()
+    photo = Photo.objects.filter(id=pk)
+    
+    return render(request, 'photos/photo.html', {'photo':photo})
 
 def addPhoto(request):
-    return render(request, 'photos/add.html')
+    categories = Category.objects.all()
+    
+    context = {'categories': categories}
+    return render(request, 'photos/add.html', context)
